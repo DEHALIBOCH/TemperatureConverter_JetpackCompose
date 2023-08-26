d9519bb773ecdeaa9a02b4b8923957b4f3639462
Marat <almabekov.marat@gmail.com>
2023-08-27 01:34:24 +0600

    Работа над интерфейсом приложения

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -37,15 +42,15 @@ fun PreviewMainActivity() {
     MainActivityContent()
 }
 
-var counter = 10
-
 @Composable
 private fun MainActivityContent() {
-    val celsius = remember { mutableStateOf(0) }
+    val celsius = remember { mutableStateOf(.0) }
+    val newCelsius = remember { mutableStateOf("") }
 
     Column {
         Header(image = R.drawable.sunrise, description = "sunrise image")
-        ConvertButton { celsius.value = counter++ }
+        EnterTemperature(temperature = newCelsius.value, changed = { newCelsius.value = it })
+        ConvertButton { newCelsius.value.toDoubleOrNull()?.let { celsius.value = it } }
         TemperatureText(celsius = celsius.value)
     }
 }
@@ -63,8 +68,8 @@ private fun Header(image: Int, description: String) {
 }
 
 @Composable
-private fun TemperatureText(celsius: Int) {
-    val fahrenheit = (celsius.toDouble() * 9 / 5) + 32
+private fun TemperatureText(celsius: Double) {
+    val fahrenheit = (celsius * 9 / 5) + 32
     Text(text = "$celsius Celsius is $fahrenheit Fahrenheit")
 }
 
@@ -73,4 +78,18 @@ private fun ConvertButton(clicked: () -> Unit) {
     Button(onClick = clicked) {
         Text(text = "Convert")
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+    TextField(
+        value = temperature,
+        onValueChange = changed,
+        label = { Text(text = "Enter a temperature in Celsius") },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Decimal
+        )
+    )
 }
